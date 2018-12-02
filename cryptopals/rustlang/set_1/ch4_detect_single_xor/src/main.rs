042cd5e1b9a30f75f5067e35d2e452d8dd8458e3
gpestana <g6pestana@gmail.com>
2018-12-02 22:52:18 +0000

    ch4: working with bug

--- cryptopals/rustlang/set_1/ch4_detect_single_xor/src/main.rs
+++ cryptopals/rustlang/set_1/ch4_detect_single_xor/src/main.rs
@@ -1,4 +1,5 @@
 extern crate cryptanalysis;
+extern crate hex;
 
 use cryptanalysis::decrypt::single_xor_decrypt;
 use cryptanalysis::frequency_analysis::{chi_square, eng_frequencies_map, utf8_occurrencies_map};
@@ -12,26 +13,31 @@ fn main() {
 
     let mut current_smaller_chi = 0.0;
     let mut current_string = String::new();
-    let mut current_secret: u8 = 10;
+    let mut current_secret: u8 = 0;
     let mut current_plaintext: Vec<u8> = Vec::new();
 
     // can this be done with map and be functional?
     loop {
         match searcher.next() {
             Some(string) => {
-                println!("decoding and processing {}", string);
-                for key in 56..90 {
-                    let string = string.to_string();
-                    let mut plaintext_candidate =
-                        single_xor_decrypt(Vec::from(string.clone()), key);
-                    let char_occ_map = utf8_occurrencies_map(&plaintext_candidate);
+                let string_hex = match hex::decode(&string) {
+                    Ok(h) => h,
+                    Err(e) => panic!(e),
+                };
 
-                    let chi = chi_square(&char_occ_map, &eng_frequencies_map());
+                for key in 0..255 {
+                    let mut plaintext_candidate =
+                        single_xor_decrypt(Vec::from(string_hex.clone()), key);
+
+                    let char_occ_map = utf8_occurrencies_map(&plaintext_candidate);
+                    let fm = eng_frequencies_map();
+
+                    let chi = chi_square(&char_occ_map, &fm);
                     if chi < current_smaller_chi || current_smaller_chi == 0.0 {
-                        current_smaller_chi = chi;
-                        current_string = string;
-                        current_secret = key;
-                        current_plaintext = plaintext_candidate;
+                        current_smaller_chi = chi.clone();
+                        current_string = string.to_string();
+                        current_secret = key.clone();
+                        current_plaintext = plaintext_candidate.clone();
                     }
                 }
             }
@@ -39,11 +45,13 @@ fn main() {
         };
     }
 
+    let decoded_string: String = current_plaintext
+        .into_iter()
+        .map(|i| i.to_string())
+        .collect::<String>();
+
     println!(
-        "\n{} {}: {} (encrypted: {})",
-        current_smaller_chi,
-        current_secret,
-        from_utf8(current_plaintext.as_slice()).unwrap(),
-        current_string
+        "\n{} {}: {:?} (encrypted: {:?})",
+        current_smaller_chi, current_secret, decoded_string, current_string
     );
 }
