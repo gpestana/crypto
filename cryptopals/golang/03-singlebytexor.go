7644058417b3e70e27da9bdc1b296517398625f0
gpestana <g6pestana@gmail.com>
2019-09-30 23:28:23 +0100

    cryptopals.go: init 03

---
+++ cryptopals/golang/03-singlebytexor.go
@@ -0,0 +1,11 @@
+// string has been XOR'd against a single character. Find the key, decrypt the
+// message.
+package main
+
+const (
+	st = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+)
+
+func singlexor() string {
+	return st
+}
--- cryptopals/golang/main.go
+++ cryptopals/golang/main.go
@@ -7,7 +7,9 @@ import (
 func main() {
 	_, r01 := hextobase64()
 	r02_ok := xor()
+	r03 := singlexor()
 
 	fmt.Println(r01)
 	fmt.Println(r02_ok)
+	fmt.Println(r03)
 }
